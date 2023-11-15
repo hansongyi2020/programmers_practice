2100d27dbd494886018edbfa06bd3dbe81603c2b
HAN SONGYI 한송이 <74229148+hansongyi2020@users.noreply.github.com>
2023-11-15 15:44:55 +0900

    [level unrated] Title: 양꼬치, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.py
@@ -0,0 +1,18 @@
+SKEWER_PRICE = 12000
+BEVERAGE_PRICE = 2000
+
+def solution(n, k):
+    validateSkewer(n)
+    validateBeverage(n, k)
+
+    answer = SKEWER_PRICE * n + BEVERAGE_PRICE * (k - n // 10) if (k - n // 10) > 0 else SKEWER_PRICE * n
+    
+    return answer
+
+def validateSkewer(n):
+    if (n < 1 and n > 999):
+        print("1 ~ 999 범위를 벗어났습니다.")
+        
+def validateBeverage(n, k):
+    if (k < n / 10 and k > 999):
+        print("{0} ~ 999 범위를 벗어났습니다.".format(n / 10))
