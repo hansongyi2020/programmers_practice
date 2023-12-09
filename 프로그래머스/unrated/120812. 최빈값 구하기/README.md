08397106c9d65eae61595f8e7cd8e7ce02d433ee
HAN SONGYI 한송이 <74229148+hansongyi2020@users.noreply.github.com>
2023-12-10 04:03:26 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/README.md
+++ 프로그래머스/unrated/120812. 최빈값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 최빈값 구하기 - 120812 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 3:43:25
+2023년 12월 0일 4:3:24
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,14 +1,8 @@
-def solution(array):
-    frequency = {}
-    for i in array:
-        try:
-            frequency[i] += 1
-        except KeyError:
-            frequency[i] = 1
+from collections import Counter
 
-    sorted_list = sorted(frequency.values())
-    if sorted_list.count(sorted_list[-1]) == 1:
-        for k, v in frequency.items():
-            if v == sorted_list[-1]:
-                return k
-    return -1
+def solution(array):
+    frequency = Counter(array).most_common(2)
+
+    if len(frequency) >= 2 and frequency[0][1] == frequency[1][1]:
+            return -1
+    return frequency[0][0]
